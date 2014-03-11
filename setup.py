c8dfdaaace860edf54392e5357b71711950729af
krieghan <krieghan.riley@gmail.com>
2014-03-11 11:26:22 -0500

    Added .swp files to .gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kobold',
+    version='1',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=['nosetests'],
+)
