90905d09a9629fbc9ca6656d0e752c1d512b7495
Krieghan J. Riley <krieghan.riley@gmail.com>
2015-06-15 14:30:54 -0500

    add assertions for things that are not responses

--- kobold/assertions.py
+++ kobold/assertions.py
@@ -1,4 +1,5 @@
 from response import response_matches
+from compare import Compare
 
 def assert_response_matches(expected,
                             response,
@@ -6,6 +7,15 @@ def assert_response_matches(expected,
     result = response_matches(expected,
                               response,
                               compare)
+    raise_if_not_match(result)
+
+def assert_equal(expected, actual, compare=None):
+    result = Compare.compare(expected, 
+                             actual,
+                             type_compare=compare)
+    raise_if_not_match(result)
+
+def raise_if_not_match(result):
     if result != 'match':
         expected_diff, actual_diff = result
         raise AssertionError(
