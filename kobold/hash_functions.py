09a395526dadac36f295674e01121818278ac91f
Krieghan J. Riley <krieghan.riley@gmail.com>
2015-07-20 17:20:49 -0500

    Add a "project" hash function, for projecting certain keys out of a dict

--- kobold/hash_functions.py
+++ kobold/hash_functions.py
@@ -1,3 +1,6 @@
+def project(hash_in, attributes):
+    return {key: value for (key, value) in hash_in.iteritems() if key in attributes}  
+
 def merge(default, to_mutate):
     for key, value in default.items():
         to_mutate.setdefault(key, value)
