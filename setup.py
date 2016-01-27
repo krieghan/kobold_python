49d7e4cc2c3433262498860cc9fcd9c00fc0b93d
Krieghan J. Riley <krieghan.riley@gmail.com>
2016-01-27 14:33:11 -0600

    Bump version to 9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kobold',
-    version='8',
+    version='9',
     packages=find_packages(),
     install_requires=['python-dateutil==2.2'],
     tests_require=['nosetests'],
