50a89b33201e1ceec8207b705b3829a72bca51c4
Krieghan J. Riley <krieghan.riley@gmail.com>
2016-02-24 16:21:59 -0600

    Bump to version 10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kobold',
-    version='9',
+    version='10',
     packages=find_packages(),
     install_requires=['python-dateutil==2.2'],
     tests_require=['nosetests'],
