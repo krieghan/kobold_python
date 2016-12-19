fe56d8c46507e330e28062e4ac7a07d09f9c2326
Krieghan J. Riley <krieghan.riley@gmail.com>
2016-12-19 16:55:32 -0600

    11.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kobold',
-    version='10.1.0',
+    version='11.0.0',
     packages=find_packages(),
     install_requires=['python-dateutil==2.2'],
     tests_require=['nosetests'],
