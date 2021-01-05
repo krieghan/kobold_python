f901d7baccc032d96dd118029a799fe939dcb90e
Krieghan J. Riley <krieghan.riley@gmail.com>
2021-01-05 16:04:59 -0600

    Change version to 11.17.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kobold',
-    version='11.16.0',
+    version='11.17.0',
     packages=find_packages(),
     install_requires=['python-dateutil']
 )
